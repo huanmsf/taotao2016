e7997ba9778ab58dca91499e23a126e9451fa79e
huanmsf <huan.xiong@yahoo.com>
2017-04-11 18:02:38 +0800

    remove taotao2016 folder all file out of control

--- taotao-manager-service/src/main/java/com/taotao/service/PictureService.java
+++
@@ -1,9 +0,0 @@
-package com.taotao.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import com.taotao.common.PictureResult;
-
-public interface PictureService {
-	public PictureResult uploadFile(MultipartFile uploadFile);
-}
