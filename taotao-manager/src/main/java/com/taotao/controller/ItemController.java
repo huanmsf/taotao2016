eb1650f8eeb584cc7e1983e67f001ca92f070f0b
huanmsf <huan.xiong@yahoo.com>
2017-04-24 14:19:50 +0800

    solr

--- taotao-manager/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager/src/main/java/com/taotao/controller/ItemController.java
@@ -1,15 +1,14 @@
 package com.taotao.controller;
 
+import com.taotao.common.EasyUIResult;
+import com.taotao.common.TaotaoResult;
+import com.taotao.pojo.Item;
+import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.taotao.common.EasyUIResult;
-import com.taotao.common.TaotaoResult;
-import com.taotao.pojo.Item;
-import com.taotao.service.ItemService;
 
 @Controller
 @RequestMapping("/item")
